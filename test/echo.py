be1bfe928e07d8aef7870118baaf7d1e2ca281c1
永远 <yaocoder@gmail.com>
2014-01-11 09:33:18 +0800

    add:客户端消息添加标识与服务端对应

--- test/echo.py
+++ test/echo.py
@@ -1,6 +1,9 @@
 __author__ = 'yaocoder'
 from socket import *
 
+TOKEN_LENGTH = 5
+TOKEN_STR = "12345"
+
 sfd = socket(AF_INET, SOCK_STREAM)
 
 ip = '192.168.14.234'
@@ -9,7 +12,7 @@ sfd.connect((ip, port))
 
 ch = ['yaocoder', 'wht', 'xty', 'zfd']
 for i in ch:
-    message = 'hello world!' + i + '\r\n'
+    message = TOKEN_STR + 'hello world!' + i + '\r\n'
     sfd.send(message)
     data = sfd.recv(20)
     print 'the data received is ',data
